85285dda366252ae1d84d0a171e598f692a51dc7
Edwin Cloud <ecloud412@gmail.com>
2018-08-29 18:49:35 -0700

    Users can edit and update reviews

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// VARIABLES AND CONSTANTS
+// REQUIREMENTS
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+const Handlebars = require('handlebars');
+const HandlebarsIntl = require('handlebars-intl');
 var exphbs = require('express-handlebars');
 
+// EXPRESS APP
+const app = express();
 
 // REVIEW MONGODB MODEL
 const Review = mongoose.model('Review', {
@@ -15,10 +19,25 @@ const Review = mongoose.model('Review', {
 });
 
 // CONFIGURATION
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+var hbs = exphbs.create({extname: '.handlebars'});
+app.engine(hbs.extname, hbs.engine);
+app.set('view engine', hbs.extname);
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true});
+
+// HANDLEBARS HELPERS
+Handlebars.registerHelper("select", function(value, options) {
+  return options.fn(this)
+    .split('\n')
+    .map(function(v) {
+      var t = 'value="' + value + '"'
+      return ! RegExp(t).test(v) ? v : v.replace(t, t + ' selected="selected"')
+    })
+    .join('\n')
+});
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 // INDEX ROUTE
 app.get('/', (req, res) => {
@@ -53,6 +72,24 @@ app.get('/reviews/:id', (req, res) => {
   }).catch((err) => {
     console.log(err.message);
   })
+});
+
+// EDIT
+app.get('/reviews/:id/edit', function (req, res) {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
+});
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
 });
 
 // CONSOLE
