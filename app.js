a1c25e2263d5dddd62c500e71d03dd2eae4545a9
Edwin Cloud <ecloud412@gmail.com>
2018-08-29 17:04:31 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+// VARIABLES AND CONSTANTS
 const express = require('express')
 const app = express()
-var exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true});
+const bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
-// OUR MONGODB MODEL
+
+// REVIEW MONGODB MODEL
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
 
+// CONFIGURATION
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true }))
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true});
 
+// INDEX ROUTE
 app.get('/', (req, res) => {
   Review.find()
     .then(reviews => {
@@ -22,6 +30,22 @@ app.get('/', (req, res) => {
     })
 })
 
+// NEW REVIEW ROUTE
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// CONSOLE
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
