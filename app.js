6a9f432586ef6162204287d7f312ccd2af8a1f33
Edwin Cloud <ecloud412@gmail.com>
2018-08-29 17:43:19 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // VARIABLES AND CONSTANTS
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
@@ -17,7 +17,7 @@ const Review = mongoose.model('Review', {
 // CONFIGURATION
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true});
 
 // INDEX ROUTE
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
     })
-})
+});
 
 // NEW REVIEW ROUTE
 app.get('/reviews/new', (req, res) => {
@@ -39,14 +39,23 @@ app.get('/reviews/new', (req, res) => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+});
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
-})
+});
 
 // CONSOLE
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
-})
+});
